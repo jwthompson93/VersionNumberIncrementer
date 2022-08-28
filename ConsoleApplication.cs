6b32022746794330da19bd439fb80ae2b45cddd2
James Thompson <james.thompson@informationcatalyst.com>
2022-08-28 16:48:11 +0100

    commandlineparser implemented in code but currently not working correctly

--- ConsoleApplication.cs
+++ ConsoleApplication.cs
@@ -1,4 +1,6 @@
 ﻿
+using CommandLine;
+using VersionNumberIncrementer.Objects;
 using VersionNumberIncrementer.Process;
 
 namespace ConsoleApplication
@@ -7,8 +9,24 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            VersionNumberProcess versionNumberIncrementer = new VersionNumberProcess();
-            versionNumberIncrementer.Process();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(Run);
+        }
+
+        private static void Run(Options opts)
+        {
+            string version = opts.version.ToLower();
+            if (version.Equals("major") || version.Equals("minor"))
+            {
+                VersionNumberProcess versionNumberProcess = new VersionNumberProcess();
+                versionNumberProcess.Process(version);
+            }
+            else
+            {
+                Console.WriteLine("ERROR: --version option is required to be either (major | minor)");
+                Console.WriteLine("Exiting...");
+                System.Environment.Exit(0);
+            }
         }
     }
 }
---
+++ Process/VersionNumberProcess.cs
@@ -0,0 +1,71 @@
+﻿using VersionNumberIncrementer.Handler;
+using VersionNumberIncrementer.Objects;
+using VersionNumberIncrementer.Splitter;
+
+namespace VersionNumberIncrementer.Process
+{
+    public class VersionNumberProcess
+    {
+        private VersionNumber versionNumberObject;
+        private VersionNumberHandler versionNumberHandler;
+
+        public VersionNumberProcess()
+        {
+            versionNumberHandler = new FileVersionNumberHandler("..\\..\\..\\ProductInfo.txt");
+        }
+
+        public void Process(string version)
+        {
+            // Obtains the Version Number from the source
+            string versionNumberString = GetVersionNumber();
+
+            // Splits the version number into an integer array
+            int[] versionNumberArray = SplitVersionNumber(versionNumberString);
+
+            // Places the integer array into a VersionNumber object
+            versionNumberObject = new VersionNumber(versionNumberArray);
+
+            IncrementVersionNumber(version);
+
+            
+        }
+
+        // Gets the version number from the file specified
+        private string GetVersionNumber()
+        {
+            string versionNumber = versionNumberHandler.ReadVersionNumber();
+            Console.WriteLine("Current Version: " + versionNumber);
+
+            return versionNumber;
+        }
+
+        // Splits the version number in an array to be processed
+        private int[] SplitVersionNumber(string versionNumber)
+        {
+            VersionNumberSplitter versionNumberSplitter = new VersionNumberSplitter();
+            int[] versionNumberArray = versionNumberSplitter.SplitVersionNumber(versionNumber, '.');
+            return versionNumberArray;
+        }
+
+        // Increments the version number based on the parameters passed
+        private void IncrementVersionNumber(string version)
+        {
+            if(version.ToLower().Equals("major"))
+            {
+                versionNumberObject.incrementMajorVersion();
+            }
+            else if(version.ToLower().Equals("minor"))
+            {
+                versionNumberObject.incrementMinorVersion();
+            }
+
+            Console.WriteLine("New Version: " + versionNumberObject.getFormattedVersionNumber());
+        }
+
+        // Writes the version number to the file
+        private void WriteVersionNumber()
+        {
+            bool isWritten = versionNumberHandler.WriteVersionNumber(versionNumberObject.getFormattedVersionNumber());
+        }
+    }
+}
